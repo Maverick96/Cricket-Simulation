f9e04d386a7da719cd460043af8e84ae0325715d
Harsh Pancholi <harshpancholi8@gmail.com>
2019-09-13 09:47:36 +0530

    Config file to change players, weights, etc

---
+++ config.js
@@ -0,0 +1,133 @@
+module.exports = {
+    wicketsLeft: 3,
+    runsRequired: 50,
+    oversLeft: 4,
+    playerNames: ['Kirat Boli', 'N.S Nodhi', 'R Rumrah', 'Shashi Hendra'],
+    weights: {
+        'Kirat Boli': [{
+            weight: 5,
+            value: 0
+        }, {
+            weight: 30,
+            value: 1
+        },
+        {
+            weight: 25,
+            value: 2
+        },
+        {
+            weight: 10,
+            value: 3
+        },
+        {
+            weight: 15,
+            value: 4
+        },
+        {
+            weight: 1,
+            value: 5
+        },
+        {
+            weight: 9,
+            value: 6
+        },
+        {
+            weight: 5,
+            value: -1
+        }],
+        'N.S Nodhi': [{
+            weight: 10,
+            value: 0
+        }, {
+            weight: 40,
+            value: 1
+        },
+        {
+            weight: 20,
+            value: 2
+        },
+        {
+            weight: 5,
+            value: 3
+        },
+        {
+            weight: 10,
+            value: 4
+        },
+        {
+            weight: 1,
+            value: 5
+        },
+        {
+            weight: 4,
+            value: 6
+        },
+        {
+            weight: 10,
+            value: -1
+        }],
+
+        'R Rumrah': [{
+            weight: 20,
+            value: 0
+        }, {
+            weight: 30,
+            value: 1
+        },
+        {
+            weight: 15,
+            value: 2
+        },
+        {
+            weight: 5,
+            value: 3
+        },
+        {
+            weight: 5,
+            value: 4
+        },
+        {
+            weight: 1,
+            value: 5
+        },
+        {
+            weight: 4,
+            value: 6
+        },
+        {
+            weight: 20,
+            value: -1
+        }],
+        'Shashi Hendra': [{
+            weight: 30,
+            value: 0
+        }, {
+            weight: 25,
+            value: 1
+        },
+        {
+            weight: 5,
+            value: 2
+        },
+        {
+            weight: 0,
+            value: 3
+        },
+        {
+            weight: 5,
+            value: 4
+        },
+        {
+            weight: 1,
+            value: 5
+        },
+        {
+            weight: 30,
+            value: 6
+        },
+        {
+            weight: 10,
+            value: -1
+        }]
+    }
+};
